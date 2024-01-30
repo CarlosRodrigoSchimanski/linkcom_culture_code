a9deea65b2645fd37f2ef9282a9d980bff49388d
CarlosRodrigoSchimanski <76493107+CarlosRodrigoSchimanski@users.noreply.github.com>
2024-01-30 00:55:52 -0300

    barra de pesquisa funcional e feia

--- scripts/home.js
+++ scripts/home.js
@@ -31,8 +31,12 @@ const applyUserProperties = async(user) =>{ //recebe o usuario e coloca nos camp
 }
 
 const applyStoreItems = async(store,id) =>{
+    let items = store.filter((item,index)=>{
+        return index < 8
+    })
+
     const content = document.getElementById('content')
-    store.forEach(element => {
+    items.forEach(element => {
         content.innerHTML += 
         `
         <div id="card">
@@ -49,6 +53,27 @@ const showCard = async(product,user) =>{
     window.location = `../produtc/produtc.html?user=${user}&product=${product}` // quando for passar mais de um parametro não usa um segundo ? e sim um &
 }
 
+const search = (store,user) =>{
+    const inputSearch = document.getElementById('inputSearch')// pego o input para verificar os eventos
+    const divSearch = document.getElementById('search2')// pego a div que vou inserir o html
+    let itens = []
+
+    inputSearch.addEventListener('keydown', (event) =>{ // crio um evento do tipo key
+        let filter = event.target.value.toUpperCase()// pego o valor digitado e dou um upercase 
+        divSearch.innerHTML = ''
+
+        let products = store.filter((item, index)=>{ // filtro todos os produtos
+            return item.name.toUpperCase().indexOf(filter)>-1
+        })
+        products.forEach(item => {
+            divSearch.innerHTML += `<button id='buttonPesquisa' onclick="showCard('${item.id}','${user.id}')">${item.name}</button>`
+        })
+        if(inputSearch.value === ''){
+            divSearch.innerHTML = ''
+        }
+    })
+}
+
 const loadData = async() =>{
     const parametros = new URLSearchParams(window.location.search)
     const id = parametros.get('id')
@@ -57,6 +82,7 @@ const loadData = async() =>{
     applyUserProperties(user)
     const store = await loadElement(`http://localhost:3000/Store`)
     applyStoreItems(store,id)
+    search(store,user)
 }
 
 loadData()
